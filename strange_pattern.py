63334cd0312a9ac09d74fe85dccc865fa1bed584
SLuelf <soenke-laurens@gmx.de>
2023-05-13 11:06:05 +0200

    task one completed

--- strange_pattern.py
+++ strange_pattern.py
@@ -2,12 +2,18 @@ import numpy as np
 
 # implement the function strange pattern
 
-def strange_pattern():
-    # delete the NotImplementedError when you write your function.
-    raise NotImplementedError
+def strange_pattern(shape = (3,3)):
+    pattern = np.zeros(shape, dtype=bool)
+    pattern[::3,::3] = True
+    pattern[1::3,2::3] = True
+    pattern[2::3,1::3] = True
+    return pattern
 
 
 if __name__ == "__main__":
     # use this for your own testing!
+    print(type(strange_pattern((10,10)))is np.ndarray)
+    print(strange_pattern((5,8)))
+    print(strange_pattern((2,2)))
 
     pass
